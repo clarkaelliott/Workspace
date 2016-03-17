5ba3fc7d6e91295a7616c7f573aac0d05a86f142
clarkaelliott <clarkaelliott@gmail.com>
2016-03-17 02:30:32 -0500

    Programming Exercise
    Initial Commit
    
    Consolidated code from previously built versions of the same respective
    exercises.
    
    3/17/2016 - 2:30 AM

---
+++ Java/Introduction to Java Programming/Chapter 1/DisplayThreeMessages.java
@@ -0,0 +1,32 @@
+
+/**
+  * DisplayThreeMessages.java
+  *  
+  * Author: Clark Elliott  |  Last Updated: Mar 17, 2016
+  * 
+  *
+  * Chapter 1.1 - (Display Three Messages)
+  * 
+  * Instructions: 
+  * 	Display the following Three Messages
+  * 		"Welcome to Java"
+  * 		"Welcome to Computer Science"
+  * 		"Programming is Fun"
+  * 
+  * Notes:
+  * 	 
+  */
+
+
+public class DisplayThreeMessages {
+	
+	public static void main(String[] args) {
+		
+		// Display Three Literals to Console
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is Fun");
+
+	}
+
+}
