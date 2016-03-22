3c615be0f134ab84e1b7a92d55129bad6656e72a
clarkaelliott <clarkaelliott@gmail.com>
2016-03-21 22:04:59 -0500

    Old Homework (Needs Updated)
    These are my old COSC 1336 assignments, I copy/pasted the files into a
    new project, need to go back and grade them, then update them.
    
    3/21/2016 @ 10:04pm

---
+++ Java/Lamar University/COSC 1336/homework_1/main.java
@@ -0,0 +1,23 @@
+package homework_1;
+
+/* Clark Elliott - 8/27/2014
+ * COSC 1336 - Fall 2014
+ * Homework #1
+ * "Printing Your Schedule"
+ */
+
+public class main {
+	public static void main(String[] args) {
+		
+		//Course Schedule, 1 Per Line; Printed to Console
+		//Formatting is COURSE #### Building Room# Start-Time Days
+		
+		System.out.println("Clark Elliott");
+		System.out.println("COSC 1172 Web TBA TBA TBA");
+		System.out.println("COSC 1173 Web TBA TBA TBA");
+		System.out.println("COSC 1336 Maes 107 9:10 MWF");		
+		System.out.println("MUSI 1306 Music 100 11:30 MWF");
+		System.out.println("MATH 1414 Galloway 124 1:50 MW");
+	}
+
+}
