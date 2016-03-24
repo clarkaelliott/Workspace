f237eb5e3e924d1261f2e9d60de0a7ec16b367fa
clarkaelliott <clarkaelliott@gmail.com>
2016-03-24 17:58:45 -0500

    Book Examples
    3/24/2016 @ 5:58pm

---
+++ Java/Introduction to Java Programming/Chapter 3 - Book Examples/ChineseZodiac.java
@@ -0,0 +1,5 @@
+public class ChineseZodiac {
+	public static void main(String[] args) {
+		// TODO
+	}
+}
---
+++ Java/Introduction to Java Programming/Chapter 3 - Book Examples/Lottery.java
@@ -0,0 +1,19 @@
+
+/**
+  * Lottery.java
+  *  
+  * Author: Clark Elliott  |  Last Updated: Mar 24, 2016
+  * 
+  *
+  * Chapter TODO - (Lottery)
+  * 
+  * Instructions:
+  * 	TODO
+  * 
+  * Notes:
+  * 	TODO
+  */
+
+public class Lottery {
+
+}
