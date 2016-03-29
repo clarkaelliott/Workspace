8c61acb53e334330cee35dc4dc0dbc25eb47168c
clarkaelliott <clarkaelliott@gmail.com>
2016-03-28 20:09:36 -0500

    Restructured Folders

---
+++ Java/Introduction to Java Programming/Chapter 5 - Programming Exercises/KilogramsToPounds.java
@@ -0,0 +1,19 @@
+
+/**
+  * KilogramsToPounds.java
+  *  
+  * Author: Clark Elliott  |  Last Updated: Mar 28, 2016
+  * 
+  *
+  * Chapter TODO - (KilogramsToPounds)
+  * 
+  * Instructions:
+  * 	TODO
+  * 
+  * Notes:
+  * 	TODO
+  */
+
+public class KilogramsToPounds {
+
+}
